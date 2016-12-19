90cc3dc2bdbf7bb43076f1f658291ccf5c93a862
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2016-12-19 09:52:21 -0500

    Control script exit. Added optional sound (exit).

--- ProteinSubcellularLocalization/submitBatch2iLocEuk.py
+++ ProteinSubcellularLocalization/submitBatch2iLocEuk.py
@@ -13,17 +13,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import os.path
+import os
+import sys
 
 parser = argparse.ArgumentParser(description='Run iLoc-Euk for a set of proteins')
 parser.add_argument('-o','--out', dest='out', metavar='file.tab', type=str, help='Output with proteins and corresponding locations', required=True)
 parser.add_argument('-f','--fasta', dest='fasta', metavar='proteome.fasta', type=str, help='FASTA with proteome', required=True)
 parser.add_argument('-t','--time', dest='wTime', metavar='N', type=int, help='Integer with waiting time for each request (it depends on the internet connect and server responsinevess)', required=True)
+parser.add_argument('-s','--sound', dest='sound', action='store_true', help='Make sound if the script finished', required=False)
 
 listOfProteinsWithSubLoc=[]
 
 args = parser.parse_args()
 fastaOBJ = open(args.fasta,"r")
 waitingTime = args.wTime
+makeSound=False
+
+# Control sound
+if args.sound:
+ makeSound=args.sound
 
 if os.path.isfile(args.out):
  outOBJ = open(args.out,"r")
@@ -70,14 +78,28 @@ for seq_record in SeqIO.parse(fastaOBJ, "fasta"):
    outOBJ.write("%s\t%s" % (seqIdentifier,res))
    outOBJ.write("\n")
    outOBJ.close()
+   listOfProteinsWithSubLoc.append(seqIdentifier)
  elif regex2.search(results_text):
   for res in regex2.findall(results_text):
    outOBJ = open(args.out,"a")
    outOBJ.write("%s\t%s" % (seqIdentifier,res))
    outOBJ.write("\n")
    outOBJ.close()
+   listOfProteinsWithSubLoc.append(seqIdentifier)
  else:
   print("Something weird happened. Check online result for protein %s" % seqIdentifier)
+  if makeSound:
+   os.system("/usr/bin/canberra-gtk-play --id='bell' --loop=10") 
+  sys.exit()
+ if seqIdentifier in listOfProteinsWithSubLoc:
+  continue
+ else:
+  print("There might be something weird with the server - protein %s was not added to output file!" % (seqIdentifier))
+  if makeSound:
+   os.system("/usr/bin/canberra-gtk-play --id='bell' --loop=10")
+  sys.exit()
 
 driver.quit()
 fastaOBJ.close()
+if makeSound:
+ os.system("/usr/bin/canberra-gtk-play --id='bell' --loop=10")
