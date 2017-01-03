b964b1c5c194d325fb66aea7b815d3631a34ddc6
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-01-03 13:46:57 -0500

    Allowed the script to import .mat files

--- COBRApy/runFBA.py
+++ COBRApy/runFBA.py
@@ -10,17 +10,29 @@ if sys.version_info[0] != 2 or sys.version_info[1] < 7 or sys.version_info[2] < 
 
 parser = argparse.ArgumentParser(description='Flux Balance Analysis', add_help=True)
 parser.add_argument('-m','--model', dest='model', metavar='importedModel', type=str, help='Model (SBML3)', required=True)
+parser.add_argument('-t','--model_type', dest='fileformat', metavar='formatFile', type=str, help='Model file format (sbml, mat)', required=True)
 
 # Get information from the argparse (arguments)
 args = parser.parse_args()
-impModel = cobra.io.read_sbml_model(args.model)
+if args.fileformat == 'sbml':
+ impModel = cobra.io.read_sbml_model(args.model)
+elif args.fileformat == 'mat':
+ impModel = cobra.io.load_matlab_model(args.model)
+else:
+ print("Something wrong...")
+ exit(1)
 
 # Print number of reactions, metabolites, and genes in model
 print("Number of Reactions: %d" % len(impModel.reactions))
 print("Number of Metabolites: %d" % len(impModel.metabolites))
 print("Number of Genes: %d" % len(impModel.genes))
 
-# Print reactions and corresponding metabolites
+# Run Optimize for the current objective function
+impModel.optimize()
+print(impModel.solution.status)
+print(impModel.solution.f)
+
+#Print reactions and corresponding metabolites
 for reaction in impModel.reactions:
  for metabolite in reaction.metabolites:
   print("%s\t%s" % (reaction, metabolite))
