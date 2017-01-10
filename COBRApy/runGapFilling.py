e370af2c2af6ea37174c12ec53eb4096d92cbda8
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-01-10 15:25:03 -0500

    Fixed description in parsed arguments

--- COBRApy/runGapFilling.py
+++ COBRApy/runGapFilling.py
@@ -9,7 +9,7 @@ if sys.version_info[0] != 2 or sys.version_info[1] < 7 or sys.version_info[2] < 
     print("This script requires Python version 2.7.8")
     sys.exit(1)
 
-parser = argparse.ArgumentParser(description='Flux Balance Analysis', add_help=True)
+parser = argparse.ArgumentParser(description='Gap Filling Analysis with COBRApy (growMatch)', add_help=True)
 parser.add_argument('-m1','--model1', dest='model1', metavar='importedModel1', type=str, help='Model (SBML3 or MAT)', required=True)
 parser.add_argument('-m2','--model2', dest='model2', metavar='importedModel2', type=str, help='Model (SBML3 or MAT)', required=True)
 parser.add_argument('-t1','--model1_type', default='sbml', dest='fileformat1', metavar='formatFile1', type=str, help='Model file format (sbml, mat)', required=False)
