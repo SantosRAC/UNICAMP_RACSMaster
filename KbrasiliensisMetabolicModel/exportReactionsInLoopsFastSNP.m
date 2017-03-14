92e048f2acc3324935a0bf13bcdc5f5b6ad49fe4
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-03-14 14:01:59 -0400

    Update on exportReactionsInLoopsFastSNP.m based on number of loops current existing in the model

--- KbrasiliensisMetabolicModel/exportReactionsInLoopsFastSNP.m
+++ KbrasiliensisMetabolicModel/exportReactionsInLoopsFastSNP.m
@@ -1,6 +1,6 @@
 % Function that iterate through loop laws and print reactions in each loop
-fid=fopen('outReactionsInLoops03062017_3.txt','w');
-for i = 1:355
+fid=fopen('outReactionsInLoops03142017.txt','w');
+for i = 1:335
     a = transpose(model_Kbr.rxns(find(Nsnp([1:end],i))));
     b = transpose(find(Nsnp([1:end],i)));
     for j = 1:length(a)
