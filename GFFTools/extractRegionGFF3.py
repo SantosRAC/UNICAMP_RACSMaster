e1b2f761a9eb9acfcb9a3df50dab95e4f7539d88
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2018-05-19 17:59:31 -0300

    Importing sys

--- GFFTools/extractRegionGFF3.py
+++ GFFTools/extractRegionGFF3.py
@@ -1,12 +1,9 @@
+import sys
 import argparse
 from Bio import SeqIO
 import re
 
-__author__      = "Renato Augusto Correa dos Santos"
-__copyright__   = "Copyright 2017"
-__license__     = "GPL v3.0"
-__email__       = "renatoacsantos@gmail.com"
-
+# Extracts regions from genes in FASTA/GFF3 annotation file
 version=sys.argv[0] + 'v.1'
 parser = argparse.ArgumentParser(description='Extract regions from FASTA and GFF3 (annotation)', add_help=True)
 parser.add_argument('-gff','--gff', dest='GFF3', metavar='genome.gff3', type=str, help='Annotation file (GFF3)', required=True)
@@ -16,6 +13,9 @@ parser.add_argument('-o','--out', dest='OUTFASTA', metavar='out.fasta', type=str
 args = parser.parse_args()
 inFastaOBJ = args.INFASTA
 outFastaOBJ = args.OUTFASTA
+
+# Open FASTA with chromosomes
+open(inFastaOBJ,"r")
 
 for seq_record in SeqIO.parse(inFastaOBJ, "fasta"):
  seqIdentifier = seq_record.id
@@ -33,25 +33,30 @@ for seq_record in SeqIO.parse(inFastaOBJ, "fasta"):
      if 0 <= (int(geneinit)-2)-1500 < len(seqItself):
       gene_sequps=seqItself[(int(geneinit)-1)-1500:(int(geneinit)-1)]
       lenseq=len(gene_sequps)
-      print(">%s\n%s" % (notes,gene_sequps))
+      outFastaOBJ.write(">%s\n%s" % (notes,gene_sequps))
+      outFastaOBJ.write("\n")
      else:
       gene_sequps=seqItself[0:(int(geneinit)-1)]
       lenseq=len(gene_sequps)
-      print(">%s\n%s" % (notes,gene_sequps))
+      outFastaOBJ.write(">%s\n%s" % (notes,gene_sequps))
+      outFastaOBJ.write("\n")
     elif strand == '-':
      if 0 <= (int(geneend)+1)+1500 < len(seqItself):
       gene_sequps=seqItself[int(geneend):(int(geneend))+1500]
       gene_sequps_revcomp = gene_sequps.reverse_complement()
       lenseq=len(gene_sequps_revcomp)
-      print(">%s\n%s" % (notes,gene_sequps_revcomp))
+      outFastaOBJ.write(">%s\n%s" % (notes,gene_sequps_revcomp))
+      outFastaOBJ.write("\n")
      else:
       gene_sequps=seqItself[int(geneend):-1]
       gene_sequps_revcomp = gene_sequps.reverse_complement()
       lenseq=len(gene_sequps_revcomp)
-      print(">%s\n%s" % (notes,gene_sequps_revcomp))
+      outFastaOBJ.write(">%s\n%s" % (notes,gene_sequps_revcomp))
+      outFastaOBJ.write("\n")
     else:
      print("Something is wrong.")
      exit(1)
  gffOBJ.close()
 
 inFastaOBJ.close()
+outFastaOBJ.close()
