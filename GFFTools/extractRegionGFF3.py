41184c6b6b6a9b9087b3e82151f3be1a103d1c4c
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2018-05-19 17:33:32 -0300

    Added a parser to the script that extracts gene upstream regions

--- GFFTools/extractRegionGFF3.py
+++ GFFTools/extractRegionGFF3.py
@@ -1,13 +1,27 @@
+import argparse
 from Bio import SeqIO
 import re
 
-fastaOBJ = open("Trichoderma_reesei_RUTC30.fasta","r")
+__author__      = "Renato Augusto Correa dos Santos"
+__copyright__   = "Copyright 2017"
+__license__     = "GPL v3.0"
+__email__       = "renatoacsantos@gmail.com"
 
-for seq_record in SeqIO.parse(fastaOBJ, "fasta"):
+version=sys.argv[0] + 'v.1'
+parser = argparse.ArgumentParser(description='Extract regions from FASTA and GFF3 (annotation)', add_help=True)
+parser.add_argument('-gff','--gff', dest='GFF3', metavar='genome.gff3', type=str, help='Annotation file (GFF3)', required=True)
+parser.add_argument('-g','--genome', dest='INFASTA', metavar='genome.fasta', type=str, help='Chromosomal sequences (FASTA)', required=True)
+parser.add_argument('-o','--out', dest='OUTFASTA', metavar='out.fasta', type=str, help='Output gene upstream regions (FASTA)', required=True)
+
+args = parser.parse_args()
+inFastaOBJ = args.INFASTA
+outFastaOBJ = args.OUTFASTA
+
+for seq_record in SeqIO.parse(inFastaOBJ, "fasta"):
  seqIdentifier = seq_record.id
  seqItself = seq_record.seq
- gffOBJ = open("TrireRUTC30_1.filtered_proteins.FilteredModels1.gff3","r")
- for line in gffOBJ:
+ inGFFOBJ = open(args.GFF3,"r")
+ for line in inGFFOBJ:
   regex1 = re.compile(r"^#")
   if regex1.search(line):
    continue
@@ -15,9 +29,7 @@ for seq_record in SeqIO.parse(fastaOBJ, "fasta"):
    scaff,prediction,feattype,geneinit,geneend,frame,strand,misc,notes=line.split("\t")
    notes=notes.rstrip("\n")
    if (feattype == 'mRNA') and (scaff == seqIdentifier):
-    #print("%s\t%s\t%s\t%s\t%s" % (scaff,geneinit,geneend,strand,notes)).rstrip("\n")
     if strand == '+':
-     #continue
      if 0 <= (int(geneinit)-2)-1500 < len(seqItself):
       gene_sequps=seqItself[(int(geneinit)-1)-1500:(int(geneinit)-1)]
       lenseq=len(gene_sequps)
@@ -42,4 +54,4 @@ for seq_record in SeqIO.parse(fastaOBJ, "fasta"):
      exit(1)
  gffOBJ.close()
 
-fastaOBJ.close()
+inFastaOBJ.close()
